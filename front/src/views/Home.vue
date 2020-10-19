57951a2b5d0ba09867bf3a410cde3c38fc3c7572
sanioooook <sahok6264@gmail.com>
2020-10-19 23:09:14 +0300

    migration from vue2 to vue3

--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,22 +1,18 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <hello-world msg="Welcome to Your Vue.js App from HelloWorld"/>
-    <hello-world-one-file
-      msg="Welcome to Your Vue.js App from HelloWorld one file component (OFC)"
-    ></hello-world-one-file>
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import { HelloWorld, HelloWorldOneFile } from '@components';
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld,
-    HelloWorldOneFile
+    HelloWorld
   }
 };
 </script>
--- front/vue.config.js
+++ front/vue.config.js
@@ -1,12 +1,24 @@
-var path = require('path');
+const path = require('path');
 
 module.exports = {
+  transpileDependencies: [
+    'vuetify'
+  ],
   chainWebpack: config => {
     config.resolve.alias
       .set('@components', path.resolve('src/components/'))
       .set('@views', path.resolve('src/views/'))
       .set('@router', path.resolve('src/router/'))
       .set('@store', path.resolve('src/store/'))
-      .set('@styles', path.resolve('src/styles/'));
-  },
+      .set('@styles', path.resolve('src/styles/'))
+      .set('@services', path.resolve('src/services/'))
+      .set('@layout', path.resolve('src/layouts/'))
+      .set('@locales', path.resolve('src/locales/'))
+      .set('@assets', path.resolve('src/assets/'))
+      .set('@filters', path.resolve('src/filters/'))
+      .set('@config', path.resolve('src/config/config.dev.json'))
+      .set('@api', path.resolve('src/services/api-service.service.js'))
+      .set('@constants', path.resolve('src/constants/'))
+      .set('@mixins', path.resolve('src/mixins/'));
+  }
 };
